462ddaec1e832ac8b2e22defbea6f78da08d659d
rimakii <166918770+rimakii@users.noreply.github.com>
2024-04-29 15:05:40 +0300

    Update register and login main.py

--- competitioner_bot/main.py
+++ competitioner_bot/main.py
@@ -2,21 +2,22 @@ import telebot
 import ast
 import time
 from telebot import types
-
+import pickle
 
 # модуль для работы с базой данныхq
 import sqlite3
 from datetime import datetime, timezone, timedelta
 
-
 bot = telebot.TeleBot("6661527759:AAGXDtVjWVPnhsBDVFFyP9DN_uzc5cCJno8")
-
-
 
 
 # Создайте таблицу пользователей, если она еще не существует
 conn = sqlite3.connect("users.db")
-
+cursor = conn.cursor()
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, login_chatids BLOB, saved_olymps BLOB)"""
+)
+conn.commit()
 
 olympiks = ["one", "two", "thre", "four", "five"]
 save_olympiks = []
@@ -25,6 +26,123 @@ delete_ol = ""
 username = ""
 
 
+def add_chatid(chat_id, login):
+    # Установите соединение с базой данных
+
+    conn = sqlite3.connect("users.db")
+
+    cursor = conn.cursor()
+
+    # Извлеките текущий массив login_chatids из базы данных
+    cursor.execute("SELECT login_chatids FROM users WHERE username = ?", (login,))
+    login_chatids = cursor.fetchone()[0]
+
+    # Если массив login_chatids пуст, создайте новый массив
+    if login_chatids is None:
+        login_chatids = []
+    else:
+        # Преобразуйте массив login_chatids из двоичного объекта в список Python
+        login_chatids = pickle.loads(login_chatids)
+
+    # Добавьте chat_id в список login_chatids
+    login_chatids.append(chat_id)
+
+    # Преобразуйте список login_chatids обратно в двоичный объект
+    login_chatids = pickle.dumps(login_chatids)
+
+    # Обновите поле login_chatids в базе данных
+    cursor.execute(
+        "UPDATE users SET login_chatids = ? WHERE username = ?", (login_chatids, login)
+    )
+    conn.commit()
+
+    # Закройте соединение с базой данных
+    cursor.close()
+    conn.close()
+
+
+def check_logging(chat_id, login):
+    # Установите соединение с базой данных
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    # Извлеките текущий массив login_chatids из базы данных
+    cursor.execute("SELECT login_chatids FROM users WHERE username = ?", (login,))
+    login_chatids = cursor.fetchone()[0]
+
+    # Если массив login_chatids пуст, верните False
+    if login_chatids is None:
+        return False
+    else:
+        # Преобразуйте массив login_chatids из двоичного объекта в список Python
+        login_chatids = pickle.loads(login_chatids)
+    for i in login_chatids:
+        bot.send_message(chat_id, f"{i}")
+    # Проверьте, есть ли chat_id в списке login_chatids
+    return chat_id in login_chatids
+
+
+def check_account(chat_id):
+    # Установите соединение с базой данных
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    # Извлеките все записи из таблицы users
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+
+    # Проверьте каждую запись на наличие chat_id в массиве login_chatids
+    for user in users:
+        login_chatids = user[3]
+
+        # Если массив login_chatids пустой, перейдите к следующей записи
+        if login_chatids is None:
+            continue
+
+        # Преобразуйте массив login_chatids из двоичного объекта в список Python
+        login_chatids = pickle.loads(login_chatids)
+
+        # Проверьте, есть ли chat_id в списке login_chatids
+        if chat_id in login_chatids:
+            # Если chat_id найден, верните соответствующий username
+            return user[1]
+
+    # Если chat_id не найден ни в одной записи, верните None
+    return None
+
+
+def delete_id(chat_id, username):
+    # Установите соединение с базой данных
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    # Извлеките текущий массив login_chatids для указанного username
+    cursor.execute("SELECT login_chatids FROM users WHERE username = ?", (username,))
+    login_chatids = cursor.fetchone()[0]
+
+    # Если массив login_chatids пустой, выйдите из функции
+    if login_chatids is None:
+        return
+
+    # Преобразуйте массив login_chatids из двоичного объекта в список Python
+    login_chatids = pickle.loads(login_chatids)
+
+    # Удалите chat_id из списка login_chatids
+    login_chatids.remove(chat_id)
+
+    # Преобразуйте список login_chatids обратно в двоичный объект
+    login_chatids = pickle.dumps(login_chatids)
+
+    # Обновите столбец login_chatids в базе данных
+    cursor.execute(
+        "UPDATE users SET login_chatids = ? WHERE username = ?",
+        (login_chatids, username),
+    )
+    conn.commit()
+
+    # Закройте соединение с базой данных
+    cursor.close()
+    conn.close()
 
 
 @bot.message_handler(commands=["start"])
@@ -44,18 +162,13 @@ def start_message(message):
     )
 
 
-
-
 @bot.callback_query_handler(func=lambda call: call.data == "reg")
 def register(call):
     # Запросите у пользователя логин
     bot.send_message(call.message.chat.id, "Введите свой логин:")
 
-
     # Установите состояние пользователя на "ввод логина"
     bot.set_state(call.message.chat.id, "username")
-
-
 
 
 @bot.message_handler(func=lambda message: bot.get_state(message.chat.id) == "username")
@@ -65,11 +178,8 @@ def get_username(message):
     username = message.text
     bot.send_message(message.chat.id, f"Введите пароль для логина {username}:")
 
-
     # Установите состояние пользователя на "ввод пароля"
     bot.set_state(message.chat.id, "password")
-
-
 
 
 @bot.message_handler(func=lambda message: bot.get_state(message.chat.id) == "password")
@@ -77,30 +187,51 @@ def get_password(message):
     # Получите пароль пользователя и зарегистрируйте его
     global username
     password = message.text
-    register_user(message.chat.id, username, password)
+    chat_id = message.chat.id
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT username FROM users")
+    usernames = cursor.fetchall()
+    if usernames is None:
+        users = []
+    else:
+        # Преобразуйте список кортежей в список строк
+        users = [username[0] for username in usernames]
+    check = check_account(chat_id)
+    if check == None:
+        if username in users:
 
+            user_id = check_credentials(username, password)
+            log = check_logging(chat_id, username)
 
-    # Удалите состояние пользователя
-    bot.delete_state(message.chat.id)
-
-
-    # Сообщите пользователю об успешной регистрации
-    bot.send_message(message.chat.id, "Регистрация прошла успешно!")
-
-
+            if user_id and log:
+                bot.send_message(message.chat.id, "Вы уже вошли в систему")
+            else:
+                bot.send_message(
+                    message.chat.id, "Этот логин уже занят, попробуйте снова"
+                )
+        else:
+            register_user(message.chat.id, username, password)
+            add_chatid(chat_id, username)
+            # Удалите состояние пользователя
+            bot.delete_state(message.chat.id)
+            # Сообщите пользователю об успешной регистрации
+            bot.send_message(message.chat.id, "Регистрация прошла успешно!")
+    else:
+        bot.send_message(message.chat.id, f"{check}, cначала выйдете из аккаунта")
 
 
 def register_user(user_login, username, password):
+
     # Вставьте нового пользователя в базу данных
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO users (id, username, password) VALUES (?, ?, ?)",
-        (user_login, username, password),
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        (username, password),
     )
+
     conn.commit()
-
-
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "login")
@@ -108,11 +239,8 @@ def login(call):
     # Запросите у пользователя логин
     bot.send_message(call.message.chat.id, "Введите свой логин:")
 
-
     # Установите состояние пользователя на "ввод логина"
     bot.set_state(call.message.chat.id, "username_log")
-
-
 
 
 # Обработайте ввод логина
@@ -125,11 +253,8 @@ def get_username(message):
     username = message.text
     bot.send_message(message.chat.id, f"Введите пароль для логина {username}:")
 
-
     # Установите состояние пользователя на "ввод пароля"
     bot.set_state(message.chat.id, "password_log")
-
-
 
 
 # Обработайте ввод пароля
@@ -139,28 +264,37 @@ def get_username(message):
 def get_password(message):
     # Получите пароль пользователя и проверьте его
     global username
-
-
+    chat_id = message.chat.id
     password = message.text
     user_id = check_credentials(username, password)
-
-
-    # Если пользователь авторизован
-    if user_id:
-        # Сообщите пользователю об успешной авторизации
-        bot.send_message(message.chat.id, "Вы успешно авторизовались!")
-    # Если пользователь не авторизован
+    check = check_account(chat_id)
+    if check == None:
+        # Если пользователь авторизован
+        if user_id:
+            if check_logging(chat_id, username):
+                bot.send_message(message.chat.id, "Вы уже вошли в систему")
+            else:
+                # Сообщите пользователю об успешной авторизации
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                button_add = types.KeyboardButton("Добавить олимпиаду")
+                button_mine = types.KeyboardButton("Список добавленных олимпиад")
+                markup.add(button_add, button_mine)
+                add_chatid(chat_id, username)
+                bot.send_message(
+                    message.chat.id,
+                    "Вы успешно авторизовались! Можете добавить олимпиаду или посмотреть уже сохраненные",
+                    reply_markup=markup,
+                )
+        # Если пользователь не авторизован
+        else:
+            # Сообщите пользователю о том, что данные введены неверно
+            bot.send_message(
+                message.chat.id, "Неверные данные для входа. Попробуйте еще раз."
+            )
     else:
-        # Сообщите пользователю о том, что данные введены неверно
-        bot.send_message(
-            message.chat.id, "Неверные данные для входа. Попробуйте еще раз."
-        )
-
-
+        bot.send_message(message.chat.id, f"{check}, cначала выйдете из аккаунта")
     # Удалите состояние пользователя
     bot.delete_state(message.chat.id)
-
-
 
 
 # Функция для проверки учетных данных пользователя
@@ -174,15 +308,23 @@ def check_credentials(username, password):
     result = cursor.fetchone()
     conn.commit()
 
-
     # Если пользователь найден, верните его идентификатор
     if result:
-        return result[0]
+        return True
     # Если пользователь не найден, верните None
     else:
-        return None
+        return False
 
 
+@bot.message_handler(commands=["exit"])
+def exit_message(message):
+    chat_id = message.chat.id
+    check = check_account(chat_id)
+    if check:
+        delete_id(chat_id, check)
+        bot.send_message(message.chat.id, f"{check}, вы вышли из аккаунта")
+    else:
+        bot.send_message(message.chat.id, "Для начала зайдите в аккаунт")
 
 
 @bot.message_handler(commands=["about"])
@@ -198,16 +340,12 @@ def start_message(message):
     )
 
 
-
-
 @bot.message_handler(commands=["add"])
 def start_message(message):
     markup = types.InlineKeyboardMarkup()
     for ol in olympiks:
         markup.add(types.InlineKeyboardButton(text=f"{ol}", callback_data=ol))
     bot.send_message(message.chat.id, "Список всех олимпиад:", reply_markup=markup)
-
-
 
 
 @bot.message_handler(content_types="text")
@@ -217,7 +355,6 @@ def message_reply(message):
         for ol in olympiks:
             markup.add(types.InlineKeyboardButton(text=f"{ol}", callback_data=ol))
         bot.send_message(message.chat.id, "Список всех олимпиад:", reply_markup=markup)
-
 
     elif message.text == "Список добавленных олимпиад":
         if save_olympiks:
@@ -232,7 +369,6 @@ def message_reply(message):
         else:
             bot.send_message(message.chat.id, "Список пуст 😞.")
 
-
     elif message.text == "Да":
         save_olympiks.append(olympiks[index])
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -243,14 +379,12 @@ def message_reply(message):
             message.chat.id, "Олимпиада добавлена в список 🥳.", reply_markup=markup
         )
 
-
     elif message.text == "Нет":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button_add = types.KeyboardButton("Добавить олимпиаду")
         button_mine = types.KeyboardButton("Список добавленных олимпиад")
         markup.add(button_add, button_mine)
         bot.send_message(message.chat.id, "ok", reply_markup=markup)
-
 
     elif message.text == "Удалить":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -262,15 +396,12 @@ def message_reply(message):
             message.chat.id, "Олимпиада удалена из списка.", reply_markup=markup
         )
 
-
     elif message.text == "Оставить все как есть":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button_add = types.KeyboardButton("Добавить олимпиаду")
         button_mine = types.KeyboardButton("Список добавленных олимпиад")
         markup.add(button_add, button_mine)
         bot.send_message(message.chat.id, "Ок", reply_markup=markup)
-
-
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -306,8 +437,6 @@ def callback_handler(call):
             "Добавить олимпиаду в список выбранных?",
             reply_markup=markup,
         )
-
-
 
 
 while True:
